8dfed8e0069dd7032754c0f99047d9f2c59178f0
ced-dev <marquis.cedric@hotmail.fr>
2016-03-11 23:26:49 +0000

    1.0.2

--- example/index.js
+++ example/index.js
@@ -1,1 +1,9 @@
+
 var gstreamer = require("../");
+
+gstreamer.start({
+    url: "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov?random=" + Math.random(),
+    port: 80,
+    quiet: false
+});
+
