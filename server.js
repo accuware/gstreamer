0c225ada1addc5b09aa6364a4370a73de7b56ad8
decades <foreverneilyoung@googlemail.com>
2018-08-20 15:51:08 +0200

    Sample address corrected

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 var gstreamer = require(".")
 
 gstreamer.start({
-//    url: "rtsp://192.168.188.99:8554/test",		                                // e.g. an gst-rtsp camera feed on Ubuntu
-    url: "rtsp://192.168.188.83",		                                // e.g. an gst-rtsp camera feed on Ubuntu
+    url: "rtsp://192.168.188.99:8554/test",		                                // e.g. an gst-rtsp camera feed on Ubuntu
     port: 8000,
     quality: 75,
     quiet: false
