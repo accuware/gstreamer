c434cd07f7e4cf9420e3e9098c11143cc1c2b700
decades <foreverneilyoung@googlemail.com>
2018-08-20 17:49:24 +0200

    Server port changed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var gstreamer = require(".")
 gstreamer.start({
     url: "rtsp://192.168.188.99:8554/test",		                                // e.g. an gst-rtsp camera feed on Ubuntu
     port: 9000,
-    quality: 75,
+    quality: 85,
     quiet: false
 })
 
